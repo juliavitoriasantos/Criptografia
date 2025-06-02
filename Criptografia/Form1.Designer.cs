853de571746a5e6fae3f62125b20c6f3ecc531b5
juliavitoriasantos <cruz.vitoria@aluno.ifsp.edu.br>
2025-06-02 09:11:28 -0300

    Adicionar arquivos de projeto.

---
+++ Criptografia/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Criptografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnome = new System.Windows.Forms.Label();
+            this.lbln = new System.Windows.Forms.Label();
+            this.btncrip = new System.Windows.Forms.Button();
+            this.txbn = new System.Windows.Forms.TextBox();
+            this.txtcrip = new System.Windows.Forms.TextBox();
+            this.txtdcrip = new System.Windows.Forms.TextBox();
+            this.lblcrip = new System.Windows.Forms.Label();
+            this.lbldcrip = new System.Windows.Forms.Label();
+            this.btndcrip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnome
+            // 
+            this.lblnome.AutoSize = true;
+            this.lblnome.Location = new System.Drawing.Point(288, 140);
+            this.lblnome.Name = "lblnome";
+            this.lblnome.Size = new System.Drawing.Size(0, 13);
+            this.lblnome.TabIndex = 0;
+            // 
+            // lbln
+            // 
+            this.lbln.AutoSize = true;
+            this.lbln.Location = new System.Drawing.Point(121, 110);
+            this.lbln.Name = "lbln";
+            this.lbln.Size = new System.Drawing.Size(44, 13);
+            this.lbln.TabIndex = 1;
+            this.lbln.Text = "Entrada";
+            // 
+            // btncrip
+            // 
+            this.btncrip.Location = new System.Drawing.Point(103, 227);
+            this.btncrip.Name = "btncrip";
+            this.btncrip.Size = new System.Drawing.Size(75, 23);
+            this.btncrip.TabIndex = 2;
+            this.btncrip.Text = "Criptografar";
+            this.btncrip.UseVisualStyleBackColor = true;
+            this.btncrip.Click += new System.EventHandler(this.btncrip_Click);
+            // 
+            // txbn
+            // 
+            this.txbn.Location = new System.Drawing.Point(92, 160);
+            this.txbn.Name = "txbn";
+            this.txbn.Size = new System.Drawing.Size(100, 20);
+            this.txbn.TabIndex = 3;
+            // 
+            // txtcrip
+            // 
+            this.txtcrip.Location = new System.Drawing.Point(303, 160);
+            this.txtcrip.Name = "txtcrip";
+            this.txtcrip.Size = new System.Drawing.Size(142, 20);
+            this.txtcrip.TabIndex = 4;
+            // 
+            // txtdcrip
+            // 
+            this.txtdcrip.Location = new System.Drawing.Point(527, 159);
+            this.txtdcrip.Name = "txtdcrip";
+            this.txtdcrip.Size = new System.Drawing.Size(100, 20);
+            this.txtdcrip.TabIndex = 5;
+            // 
+            // lblcrip
+            // 
+            this.lblcrip.AutoSize = true;
+            this.lblcrip.Location = new System.Drawing.Point(300, 110);
+            this.lblcrip.Name = "lblcrip";
+            this.lblcrip.Size = new System.Drawing.Size(106, 13);
+            this.lblcrip.TabIndex = 6;
+            this.lblcrip.Text = "Código Criptografado";
+            // 
+            // lbldcrip
+            // 
+            this.lbldcrip.AutoSize = true;
+            this.lbldcrip.Location = new System.Drawing.Point(524, 109);
+            this.lbldcrip.Name = "lbldcrip";
+            this.lbldcrip.Size = new System.Drawing.Size(124, 13);
+            this.lbldcrip.TabIndex = 7;
+            this.lbldcrip.Text = "Código Descriptografado";
+            // 
+            // btndcrip
+            // 
+            this.btndcrip.Location = new System.Drawing.Point(303, 227);
+            this.btndcrip.Name = "btndcrip";
+            this.btndcrip.Size = new System.Drawing.Size(92, 23);
+            this.btndcrip.TabIndex = 8;
+            this.btndcrip.Text = "Descriptografar";
+            this.btndcrip.UseVisualStyleBackColor = true;
+            this.btndcrip.Click += new System.EventHandler(this.btndcrip_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btndcrip);
+            this.Controls.Add(this.lbldcrip);
+            this.Controls.Add(this.lblcrip);
+            this.Controls.Add(this.txtdcrip);
+            this.Controls.Add(this.txtcrip);
+            this.Controls.Add(this.txbn);
+            this.Controls.Add(this.btncrip);
+            this.Controls.Add(this.lbln);
+            this.Controls.Add(this.lblnome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnome;
+        private System.Windows.Forms.Label lbln;
+        private System.Windows.Forms.Button btncrip;
+        private System.Windows.Forms.TextBox txbn;
+        private System.Windows.Forms.TextBox txtcrip;
+        private System.Windows.Forms.TextBox txtdcrip;
+        private System.Windows.Forms.Label lblcrip;
+        private System.Windows.Forms.Label lbldcrip;
+        private System.Windows.Forms.Button btndcrip;
+    }
+}
+
---
+++ Criptografia/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criptografia
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<char, int> mapaCriptografia = new Dictionary<char, int>()
+            {
+                {'A', 10}, {'B', 52}, {'C', 93}, {'D', 49}, {'E', 54}, {'F', 259}, {'G', 825}, {'H', 61}, {'I', 47}, {'J', 333}, {'L', 482}, {'M', 29}, {'N', 31}, {'O', 96}, {'P', 231}, {'Q', 799}, {'R', 568}, {'S', 178}, {'T', 873}, {'U', 993}, {'V', 22}, {'W', 76}, {'X', 37}, {'Y', 64}, {'Z', 17}
+            };
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncrip_Click(object sender, EventArgs e)
+        {
+            string texto = txbn.Text.ToUpper(); // converte para maiúsculas
+            List<string> resultados = new List<string>();
+
+            foreach (char c in texto)
+            {
+                if (mapaCriptografia.ContainsKey(c))
+                {
+                    int valor = mapaCriptografia[c];
+
+                    // Aplica a fórmula:
+                    // ((valor * 7) + 9) / 3 * 8
+
+                    double passo1 = valor * 7;
+                    double passo2 = passo1 + 9;
+                    double passo3 = passo2 / 3.0;
+                    double passo4 = passo3 * 8;
+
+                    // Arredondar Resultado
+                    int resultadoFinal = (int)Math.Round(passo4);
+
+                    resultados.Add(resultadoFinal.ToString());
+                }
+                else
+                {
+                    resultados.Add("??"); // para caracteres que não existem no dicionário
+                }
+            }
+
+            // Junta resultados separados por hífen
+            txtcrip.Text = string.Join("-", resultados);
+        }
+
+        private void btndcrip_Click(object sender, EventArgs e)
+        {
+            // Criar dicionário inverso valor → letra para facilitar busca
+            Dictionary<int, char> mapaDescriptografia = new Dictionary<int, char>();
+            foreach (var par in mapaCriptografia)
+            {
+                mapaDescriptografia[par.Value] = par.Key;
+            }
+
+            string textoCriptografado = txtcrip.Text;
+            string[] valores = textoCriptografado.Split('-');
+
+            StringBuilder textoOriginal = new StringBuilder();
+
+            foreach (string valStr in valores)
+            {
+                if (int.TryParse(valStr, out int resultado))
+                {
+                    // Aplica fórmula inversa para achar o valor original
+                    double valorOriginal = (3.0 * (resultado / 8.0) - 9) / 7.0;
+
+                    // Arredonda para o inteiro mais próximo (pois o valor original era inteiro)
+                    int valorInt = (int)Math.Round(valorOriginal);
+
+                    if (mapaDescriptografia.ContainsKey(valorInt))
+                    {
+                        textoOriginal.Append(mapaDescriptografia[valorInt]);
+                    }
+                    else
+                    {
+                        textoOriginal.Append('?'); // valor não encontrado no mapa
+                    }
+                }
+                else
+                {
+                    textoOriginal.Append('?'); // valor inválido
+                }
+            }
+
+            txtdcrip.Text = textoOriginal.ToString();
+        }
+    }
+    
+}
